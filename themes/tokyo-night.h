af3e7c661f75f44a166f96081fdeef54ec83de8e
Crispiie <austinbunn1@icloud.com>
2024-07-09 20:10:22 -0500

    Fixed Tokyo Night Theme

--- themes/tokyo-night.h
+++ themes/tokyo-night.h
@@ -10,6 +10,6 @@ static const char col_urgborder[]   = "#ff5370";   // Urgent border color
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_cyan, col_gray1, col_gray2 },  // Normal windows
-    [SchemeSel]  = { col_gray3, col_gray1, col_gray5 },  // Selected window
+    [SchemeSel]  = { col_gray3, col_gray1, col_cyan },  // Selected window
     [SchemeUrg]  = { col_gray4, col_cyan, col_urgborder },  // Urgent window
 };
