8f3cab8a47b068a353dca64c52b233a33d555e33
Crispiie <austinbunn1@icloud.com>
2024-07-10 23:52:32 -0500

    Added Catppuccin theme

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-#include "/home/crispi/suckless/dwm/themes/tokyo-night.h"
+#include "/home/crispi/suckless/dwm/themes/catppuccin.h"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
--- themes/catppuccin.h
+++ themes/catppuccin.h
@@ -1,15 +1,15 @@
-static const char col_gray1[]       = "#cdd6f4";
-static const char col_gray2[]       = "#1e1e2e";
-static const char col_gray3[]       = "#282737";
-static const char col_gray4[]       = "#74c7ec";
-static const char col_gray5[]       = "#f5e0dc";
-static const char col_urgborder[]   = "#89b4fa";
-static const char col_cyan[]        = "#89dceb";
+
+static const char col_gray1[]       = "#1D1D2D";  // Background color
+static const char col_gray2[]       = "#282737";  // Unfocused window border
+static const char col_gray3[]       = "#95CCFA";  // Foreground color (unfocused)
+static const char col_gray4[]       = "#F18EAC";  // Selected foreground color (focused)
+static const char col_cyan[]        = "#6272a4";  // Background color for urgent windows
+static const char col_gray5[]       = "#DCB5F1";  // Focused window border (pink)
+static const char col_urgborder[]   = "#ff5555";  // Urgent window border
 
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1,  col_gray5  },
-	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
-
+    /*               fg             bg             border   */
+    [SchemeNorm] = { col_gray3,     col_gray1,     col_gray2 },
+    [SchemeSel]  = { col_gray4,     col_gray1,     col_gray5 },
+    [SchemeUrg]  = { col_gray4,     col_cyan,      col_urgborder },
 };
