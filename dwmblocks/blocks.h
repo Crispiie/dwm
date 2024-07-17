7908b9e92a02e15eaa6f858028eb191558d8dceb
Crispiie <austinbunn1@icloud.com>
2024-07-17 18:54:26 -0500

    Added dwmblocks

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,18 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+	{"", "~/suckless/dwm/dwmblocks/scripts/volume",		0,		10},
+
+	{"", "~/suckless/dwm/dwmblocks/scripts/cpu",		10,		18},
+
+	{"", "~/suckless/dwm/dwmblocks/scripts/memory",		10,		2},
+
+	{"", "~/suckless/dwm/dwmblocks/scripts/battery",	30,		3},
+
+	{"", "~/suckless/dwm/dwmblocks/scripts/date",		30,		4},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
